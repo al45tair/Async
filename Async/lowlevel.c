a658ff241aa4137ecc84574be4f63f1ae4e066b0
Alastair Houghton <alastair@coriolis-systems.com>
2014-09-29 16:36:26 +0100

    Updated for Xcode 6.1.

--- Async/lowlevel.c
+++ Async/lowlevel.c
@@ -17,44 +17,44 @@
 #define ASYNC_STACK_SIZE 16384
 #define ASYNC_POOL_SIZE  (32 * ASYNC_STACK_SIZE)
 
-struct async_stack {
-  struct async_stack *next;
+struct async_ll_stack {
+  struct async_ll_stack *next;
 };
 
-struct async_arena {
-  struct async_arena *next;
+struct async_ll_arena {
+  struct async_ll_arena *next;
 };
 
-struct async_thread {
+struct async_ll_thread {
   pthread_mutex_t     q_mutex;
   pthread_cond_t      q_cond;
-  struct async_task  *ready_q;
-  struct async_task  *current_task;
+  struct async_ll_task  *ready_q;
+  struct async_ll_task  *current_task;
   dispatch_queue_t    dispatch_q;
   CFRunLoopSourceRef  source;
   CFRunLoopRef        runloop;
   
-  struct async_arena *arenas;
+  struct async_ll_arena *arenas;
   
-  struct async_arena *current_arena;
+  struct async_ll_arena *current_arena;
   uint8_t            *arena_ptr;
   uint8_t            *arena_end;
   
-  struct async_stack *free_stacks;
+  struct async_ll_stack *free_stacks;
 };
 
-struct async_task {
+struct async_ll_task {
   bool                 done;
-  struct async_thread *owner;
-  struct async_task   *next_ready;
+  struct async_ll_thread *owner;
+  struct async_ll_task   *next_ready;
   int64_t              result;
   jmp_buf              buf;
   void                *stack_base;
   void                *arg;
   int64_t             (*entry_point)(void *arg);
   CFTypeRef           retained;
-  struct async_task   *caller;
-  struct async_task   *awaiting;
+  struct async_ll_task   *caller;
+  struct async_ll_task   *awaiting;
 };
 
 static int64_t call_block(void *arg) {
@@ -62,23 +62,23 @@ static int64_t call_block(void *arg) {
   return blk();
 }
 
-static bool async_run_next(void);
-static bool async_run_all_blocking(void);
-static void async_perform(void *info);
-static struct async_thread *async_current_thread(void);
-static void async_switch_to(async_task_t task);
-static void *async_alloc_stack(struct async_thread *thread);
-static void async_release_stack(struct async_thread *thread, void *base);
+static bool async_ll_run_next(void);
+static bool async_ll_run_all_blocking(void);
+static void async_ll_perform(void *info);
+static struct async_ll_thread *async_ll_current_thread(void);
+static void async_ll_switch_to(async_ll_task_t task);
+static void *async_ll_alloc_stack(struct async_ll_thread *thread);
+static void async_ll_release_stack(struct async_ll_thread *thread, void *base);
 
-void enter_task(async_task_t task) {
+void enter_task(async_ll_task_t task) {
   task->result = task->entry_point (task->arg);
   task->done = true;
   if (task->awaiting)
-    async_switch_to (task->awaiting);
-  async_switch_to (task->caller);
+    async_ll_switch_to (task->awaiting);
+  async_ll_switch_to (task->caller);
 }
 
-int64_t call_with_stack(async_task_t task,
+int64_t call_with_stack(async_ll_task_t task,
                         void *stack_top) __attribute__((noreturn));
 
 #if defined(__x86_64__)
@@ -124,21 +124,21 @@ _call_with_stack:\n\
 #error You need to write code for your CPU
 #endif
 
-static pthread_key_t async_key;
-static void async_teardown(void *arg);
+static pthread_key_t async_ll_key;
+static void async_ll_teardown(void *arg);
 
 static void
-async_init(void)
+async_ll_init(void)
 {
-  pthread_key_create (&async_key, async_teardown);
+  pthread_key_create (&async_ll_key, async_ll_teardown);
 }
 
 static void
-async_teardown(void *arg)
+async_ll_teardown(void *arg)
 {
-  struct async_thread *thread = (struct async_thread *)arg;
+  struct async_ll_thread *thread = (struct async_ll_thread *)arg;
   
-  for (struct async_arena *arena = thread->arenas, *next_arena;
+  for (struct async_ll_arena *arena = thread->arenas, *next_arena;
        arena;
        arena = next_arena) {
     next_arena = arena->next;
@@ -151,41 +151,41 @@ async_teardown(void *arg)
   free (thread);
 }
 
-static struct async_thread *
-async_current_thread(void)
+static struct async_ll_thread *
+async_ll_current_thread(void)
 {
   static pthread_once_t once = PTHREAD_ONCE_INIT;
-  struct async_thread *thread;
+  struct async_ll_thread *thread;
   
-  pthread_once(&once, async_init);
+  pthread_once(&once, async_ll_init);
   
-  thread = pthread_getspecific (async_key);
+  thread = pthread_getspecific (async_ll_key);
   
   if (!thread) {
-    size_t size = sizeof(struct async_thread) + sizeof(struct async_task);
-    thread = (struct async_thread *)malloc (size);
+    size_t size = sizeof(struct async_ll_thread) + sizeof(struct async_ll_task);
+    thread = (struct async_ll_thread *)malloc (size);
     
     memset (thread, 0, size);
     pthread_mutex_init (&thread->q_mutex, NULL);
     pthread_cond_init (&thread->q_cond, NULL);
-    thread->current_task = (struct async_task *)(thread + 1);
+    thread->current_task = (struct async_ll_task *)(thread + 1);
     thread->current_task->owner = thread;
     
-    pthread_setspecific (async_key, thread);
+    pthread_setspecific (async_ll_key, thread);
   }
   
   return thread;
 }
 
 void
-async_schedule_in_runloop(CFRunLoopRef runLoop)
+async_ll_schedule_in_runloop(CFRunLoopRef runLoop)
 {
-  struct async_thread *thread = async_current_thread();
+  struct async_ll_thread *thread = async_ll_current_thread();
   CFRunLoopSourceContext ctx;
   
   memset (&ctx, 0, sizeof(ctx));
   
-  ctx.perform = async_perform;
+  ctx.perform = async_ll_perform;
   
   thread->runloop = runLoop;
   thread->source = CFRunLoopSourceCreate (kCFAllocatorDefault, 0, &ctx);
@@ -194,17 +194,17 @@ async_schedule_in_runloop(CFRunLoopRef runLoop)
 }
 
 void
-async_schedule_in_queue(dispatch_queue_t queue)
+async_ll_schedule_in_queue(dispatch_queue_t queue)
 {
-  struct async_thread *thread = async_current_thread();
+  struct async_ll_thread *thread = async_ll_current_thread();
 
   thread->dispatch_q = queue;
 }
 
 void
-async_unschedule()
+async_ll_unschedule()
 {
-  struct async_thread *thread = async_current_thread();
+  struct async_ll_thread *thread = async_ll_current_thread();
 
   thread->dispatch_q = NULL;
   
@@ -217,33 +217,33 @@ async_unschedule()
 }
 
 static void
-async_switch_to(async_task_t task)
+async_ll_switch_to(async_ll_task_t task)
 {
-  struct async_thread *thread = async_current_thread();
+  struct async_ll_thread *thread = async_ll_current_thread();
   thread->current_task = task;
   _longjmp (thread->current_task->buf, 1);
 }
 
-static async_task_t
-async_call_fn_impl(void *arg,
+static async_ll_task_t
+async_ll_call_fn_impl(void *arg,
                    int64_t (*pfn)(void *arg),
                    CFTypeRef retained)
 {
-  struct async_thread *thread = async_current_thread();
-  volatile struct async_task *task
-  = (struct async_task *)malloc (sizeof (struct async_task));
+  struct async_ll_thread *thread = async_ll_current_thread();
+  volatile struct async_ll_task *task
+  = (struct async_ll_task *)malloc (sizeof (struct async_ll_task));
   
-  memset ((void *)task, 0, sizeof(struct async_task));
-  task->owner = (struct async_thread *)thread;
+  memset ((void *)task, 0, sizeof(struct async_ll_task));
+  task->owner = (struct async_ll_thread *)thread;
   
   if (_setjmp(thread->current_task->buf))
-    return (async_task_t)task;
+    return (async_ll_task_t)task;
   
   task->caller = thread->current_task;
-  thread->current_task = (async_task_t)task;
+  thread->current_task = (async_ll_task_t)task;
   
   // Call with a new stack
-  volatile void *base = async_alloc_stack(thread);
+  volatile void *base = async_ll_alloc_stack(thread);
   void *top = (char *)base + ASYNC_STACK_SIZE;
   
   task->stack_base = (void *)base;
@@ -251,27 +251,27 @@ async_call_fn_impl(void *arg,
   task->arg = arg;
   task->retained = retained ? CFRetain(retained) : NULL;
   
-  call_with_stack ((async_task_t)task, top);
+  call_with_stack ((async_ll_task_t)task, top);
 }
 
 
-async_task_t
-async_call_fn(void *arg,
+async_ll_task_t
+async_ll_call_fn(void *arg,
               int64_t (*pfn)(void *arg))
 {
-  return async_call_fn_impl (arg, pfn, NULL);
+  return async_ll_call_fn_impl (arg, pfn, NULL);
 }
 
-async_task_t
-async_call(int64_t (^blk)(void))
+async_ll_task_t
+async_ll_call(int64_t (^blk)(void))
 {
-  return async_call_fn_impl (blk, call_block, blk);
+  return async_ll_call_fn_impl (blk, call_block, blk);
 }
 
-int64_t async_await(async_task_t t)
+int64_t async_ll_await(async_ll_task_t t)
 {
-  volatile async_task_t task = t;
-  volatile struct async_thread *thread = async_current_thread();
+  volatile async_ll_task_t task = t;
+  volatile struct async_ll_thread *thread = async_ll_current_thread();
   int64_t result;
   
   /* Must have the same owning thread */
@@ -285,16 +285,16 @@ int64_t async_await(async_task_t t)
            queue without being in asynchronous context */
         assert(!thread->runloop && !thread->dispatch_q);
         
-        async_run_all_blocking();
+        async_ll_run_all_blocking();
       } else {
-        async_switch_to (thread->current_task->caller);
+        async_ll_switch_to (thread->current_task->caller);
       }
     } else {
       task->awaiting = NULL;
     }
   }
   
-  async_release_stack((struct async_thread *)thread, task->stack_base);
+  async_ll_release_stack((struct async_ll_thread *)thread, task->stack_base);
   if (task->retained)
     CFRelease (task->retained);
   result = task->result;
@@ -303,17 +303,17 @@ int64_t async_await(async_task_t t)
 }
 
 void
-async_suspend()
+async_ll_suspend()
 {
-  volatile struct async_thread *thread = async_current_thread();
+  volatile struct async_ll_thread *thread = async_ll_current_thread();
   if (_setjmp (thread->current_task->buf) == 0)
-    async_switch_to (thread->current_task->caller);
+    async_ll_switch_to (thread->current_task->caller);
 }
 
 void
-async_wake(async_task_t task)
+async_ll_wake(async_ll_task_t task)
 {
-  struct async_thread *owner = task->owner;
+  struct async_ll_thread *owner = task->owner;
   pthread_mutex_lock (&owner->q_mutex);
   if (!task->next_ready) {
     if (owner->ready_q) {
@@ -327,7 +327,7 @@ async_wake(async_task_t task)
 
   if (owner->dispatch_q) {
     dispatch_async (owner->dispatch_q, ^{
-      while (async_run_next());
+      while (async_ll_run_next());
     });
   } else if (owner->source) {
     CFRunLoopSourceSignal (owner->source);
@@ -339,10 +339,10 @@ async_wake(async_task_t task)
 }
 
 static bool
-async_run_next(void)
+async_ll_run_next(void)
 {
-  volatile struct async_thread *thread = async_current_thread();
-  volatile async_task_t task = NULL;
+  volatile struct async_ll_thread *thread = async_ll_current_thread();
+  volatile async_ll_task_t task = NULL;
   
   pthread_mutex_lock ((pthread_mutex_t *)&thread->q_mutex);
   if (thread->ready_q) {
@@ -355,17 +355,17 @@ async_run_next(void)
   
   if (task && _setjmp (thread->current_task->buf) == 0) {
     task->caller = thread->current_task;
-    async_switch_to (task);
+    async_ll_switch_to (task);
   }
   
   return task;
 }
 
 static bool
-async_run_all_blocking(void)
+async_ll_run_all_blocking(void)
 {
-  volatile struct async_thread *thread = async_current_thread();
-  volatile async_task_t task = NULL;
+  volatile struct async_ll_thread *thread = async_ll_current_thread();
+  volatile async_ll_task_t task = NULL;
   volatile bool ran_one = false;
   
   pthread_mutex_lock ((pthread_mutex_t *)&thread->q_mutex);
@@ -384,37 +384,37 @@ async_run_all_blocking(void)
   if (task && _setjmp (thread->current_task->buf) == 0) {
     ran_one = true;
     task->caller = thread->current_task;
-    async_switch_to (task);
+    async_ll_switch_to (task);
   }
   
   return ran_one;
 }
 
 static void
-async_perform(__unused void *info)
+async_ll_perform(__unused void *info)
 {
-  while (async_run_next());
+  while (async_ll_run_next());
 }
 
-async_task_t
-async_current_task(void)
+async_ll_task_t
+async_ll_current_task(void)
 {
-  return async_current_thread()->current_task;
+  return async_ll_current_thread()->current_task;
 }
 
 bool
-async_done(async_task_t task)
+async_ll_done(async_ll_task_t task)
 {
   return task->done;
 }
 
 static void *
-async_alloc_stack(struct async_thread *thread)
+async_ll_alloc_stack(struct async_ll_thread *thread)
 {
   uint8_t *base;
   
   if (thread->free_stacks) {
-    struct async_stack *stack = thread->free_stacks->next;
+    struct async_ll_stack *stack = thread->free_stacks->next;
     if (stack == thread->free_stacks)
       thread->free_stacks = NULL;
     else
@@ -426,14 +426,14 @@ async_alloc_stack(struct async_thread *thread)
   }
   
   if (thread->arena_ptr + ASYNC_STACK_SIZE >= thread->arena_end) {
-    struct async_arena *the_arena;
+    struct async_ll_arena *the_arena;
     
     thread->arena_ptr = mmap (NULL, ASYNC_POOL_SIZE,
                                PROT_READ|PROT_WRITE,
                                MAP_ANON|MAP_PRIVATE, -1, 0);
     thread->arena_end = thread->arena_ptr;
     
-    the_arena = (struct async_arena *)(thread->arena_ptr);
+    the_arena = (struct async_ll_arena *)(thread->arena_ptr);
     the_arena->next = thread->current_arena;
     thread->current_arena = the_arena;
   }
@@ -450,9 +450,9 @@ async_alloc_stack(struct async_thread *thread)
 }
 
 static void
-async_release_stack(struct async_thread *thread, void *base)
+async_ll_release_stack(struct async_ll_thread *thread, void *base)
 {
-  struct async_stack *stack = (struct async_stack *)((uint8_t *)base + 4096);
+  struct async_ll_stack *stack = (struct async_ll_stack *)((uint8_t *)base + 4096);
   
   if (thread->free_stacks) {
     stack->next = thread->free_stacks->next;
--- Async/lowlevel.h
+++ Async/lowlevel.h
@@ -17,50 +17,50 @@
 /* These functions work on a per-thread basis.  That is, each thread has
    its own async. context.
  
-   If you call async_await() at top-level, i.e. outside of an async_call()
+   If you call async_ll_await() at top-level, i.e. outside of an async_ll_call()
    invocation, IT WILL BLOCK THE CURRENT THREAD UNTIL THE TASK YOU ARE
    AWAITING HAS COMPLETED.  Tasks that you started on the current thread may
-   wake and execute in the meantime, but the top-level async_await() will
+   wake and execute in the meantime, but the top-level async_ll_await() will
    remain blocked.
  
    If you are using a run loop or a dispatch queue to drive a given thread,
    you can register with those mechanisms.  In that case, YOU MUST NOT CALL
-   async_await() OUTSIDE OF AN async_call() INVOCATION as it will block the
+   async_ll_await() OUTSIDE OF AN async_ll_call() INVOCATION as it will block the
    thread and NO PROGRESS WILL BE MADE.  If you are registered with a queue
    or with a run loop, the queue/run loop will process events while your
    top-level task awaits the completion of a subtask. */
 
-typedef struct async_task *async_task_t;
+typedef struct async_ll_task *async_ll_task_t;
 
 /* Call this if you are using dispatch_main(). */
-void async_schedule_in_queue(dispatch_queue_t queue);
+void async_ll_schedule_in_queue(dispatch_queue_t queue);
 
 /* Call this if you are using a CF/NSRunLoop. */
-void async_schedule_in_runloop(CFRunLoopRef runLoop);
+void async_ll_schedule_in_runloop(CFRunLoopRef runLoop);
 
 /* Call this to undo the above */
-void async_unschedule(void);
+void async_ll_unschedule(void);
 
 /* Call a block; this also works for Swift functions. */
-async_task_t async_call(int64_t (^blk)(void));
+async_ll_task_t async_ll_call(int64_t (^blk)(void));
 
 /* Call a function, passing in the specified argument */
-async_task_t async_call_fn(void *arg,
-                           int64_t (*blk)(void *arg));
+async_ll_task_t async_ll_call_fn(void *arg,
+                                 int64_t (*blk)(void *arg));
 
 /* Wait for a given async task to complete */
-int64_t async_await(async_task_t task);
+int64_t async_ll_await(async_ll_task_t task);
 
 /* Suspend the current async task */
-void async_suspend(void);
+void async_ll_suspend(void);
 
 /* Wake the specified async task, which should be suspended */
-void async_wake(async_task_t task);
+void async_ll_wake(async_ll_task_t task);
 
 /* Get the current async task */
-async_task_t async_current_task(void);
+async_ll_task_t async_ll_current_task(void);
 
 /* Check if the specified async task is done yet */
-bool async_done(async_task_t task);
+bool async_ll_done(async_ll_task_t task);
 
-#endif /* ASYNC_H_ */
+#endif /* LOWLEVEL_H_ */
